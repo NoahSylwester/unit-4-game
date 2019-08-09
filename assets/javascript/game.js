11cdfbc4a6abb58675a6c38f5ff40687f86674a1
Noah Sylwester <nanoabram@comcast.net>
2019-08-09 01:33:54 -0700

    Added music to playstart upon character selection.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
-$('#martha').on("click",function() {
-  document.getElementById("martha-sound").play();
+$('document').ready(function(){
   
+  $('#martha').on("click",function() {
+    document.getElementById("martha-sound").play();
+    document.getElementById("music").play();
+  });
+  $('#liam').on("click",function() {
+    document.getElementById("liam-sound").play();
+    document.getElementById("music").play();
+  });
+  $('#kuratas').on("click",function() {
+    document.getElementById("kuratas-sound").play();
+    document.getElementById("music").play();
+  });
+  $('#mk-gndm').on("click",function() {
+    document.getElementById("mk-gndm-sound").play();
+    document.getElementById("music").play();
+  });
 });
-$('#liam').on("click",function() {
-  document.getElementById("liam-sound").play();
-});
-$('#kuratas').on("click",function() {
-  document.getElementById("kuratas-sound").play();
-});
-$('#mk-gndm').on("click",function() {
-  document.getElementById("mk-gndm-sound").play();
-});
-
