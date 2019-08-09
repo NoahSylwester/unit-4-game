2ab7a52e889e012e790f42f777cf0ea4f2ca1b4e
Noah Sylwester <nanoabram@comcast.net>
2019-08-09 00:58:09 -0700

    Added character selection, css animations, character select sounds.

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$('#martha').on("click",function() {
+  document.getElementById("martha-sound").play();
+  
+});
+$('#liam').on("click",function() {
+  document.getElementById("liam-sound").play();
+});
+$('#kuratas').on("click",function() {
+  document.getElementById("kuratas-sound").play();
+});
+$('#mk-gndm').on("click",function() {
+  document.getElementById("mk-gndm-sound").play();
+});
+
