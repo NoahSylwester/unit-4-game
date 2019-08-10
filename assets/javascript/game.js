de7547c39921b4528b47a6136f9c2e06a4105b31
Noah Sylwester <nanoabram@comcast.net>
2019-08-10 15:59:28 -0700

    edited html to have three sections: title-screen, enemy-select, and battle-screen. Also enable enemy appearance on gameStart().

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,52 @@
-var gameRunning = false;
+var gameRunning = false; // used to stop gameStart() from restarting
+var charArr = [$('#martha'),$('#liam'),$('#kuratas'),$('#mkgndm')];
 
-function Character(name, hp, atk, cntrAtk) {
+function Character(name, hp, atk, cntrAtk) { // define character constructor
   this.name = name;
   this.hp = hp;
   this.atk = atk;
   this.cntrAtk = cntrAtk;
 
-  this.attack = function(enemy) {
+  this.attack = function(enemy) { // player attack
     enemy.hp -= this.atk;
   };
 
-  this.counterAttack = function(player) {
+  this.counterAttack = function(player) { // computer attack
     player.hp -= this.cntrAtk;
   }
 }
 
-var martha = new Character("martha", 140, 5, 5);
+var martha = new Character("martha", 140, 5, 5); // constructing all characters
 var liam = new Character("liam", 100, 8, 8);
 var kuratas = new Character("kuratas", 200, 4, 4);
 var mkgndm = new Character("mkgndm", 120, 10, 10);
 
-gameStart = function(characterChoice) {
+gameStart = function(characterChoice) { // begins game process after character selection
   if (gameRunning === false) {
-    var player = characterChoice;
-    $('.title').addClass("fadeout");
+    var player = characterChoice; // sets chosen character to player and all others to enemy
+    var enemies = charArr.filter((enemy) => { // sets enemy array as all non-players
+      return (enemy.attr('id') !== player.attr('id'));
+    });
+  
+    $('.title').addClass("fadeout"); // fades title screen elements out
     $('.char-prompt').addClass("fadeout");
-    $('.img-thumbnail').addClass("fadeout");
-    $('.img-thumbnail').removeClass("img-hover");
-    setTimeout(() => {
+    $('.character').addClass("fadeout");
+    $('.character').removeClass("img-hover"); // removes hover from characters
+    setTimeout(() => { // wait to remove elements so that they are completely transparent before any position changes
 
       $('.title').css("display", "none");
       $('.char-prompt').css("display", "none");
       $('body').css({"display":"flex", "align-items":"center", "justify-content":"center"});
-      $('.img-thumbnail').addClass('no-display');
-      player.removeClass('no-display');
+      $('.character').addClass('no-display');
+      player.removeClass('no-display'); // display and fade in player character
       player.addClass('fadein');
+      $('.fighter-holder').append($('<div>').addClass('col-xs-4'));
+      $('.fighter-holder').append($('<div>').addClass('col-xs-5').append(enemies[0]));
+      enemies.forEach((enemy) => { // display and fade in enemy characters for selection
+        enemy.css({"background-color":"#252525", "color":"#bd1c00"});
+        enemy.removeClass('no-display');
+        enemy.addClass('fadein'); 
+       });
 
     }, 3000);
 
