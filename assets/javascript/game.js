7db5c0933ea5f0e76e35f74ecca551314e89cc14
Noah Sylwester <nanoabram@comcast.net>
2019-08-10 18:38:40 -0700

    Added all dialogue. Finished enemy select screen. Still need to enable automatic text selection and combat.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,45 @@
 var gameRunning = false; // used to stop gameStart() from restarting
 var charArr = [$('#martha'),$('#liam'),$('#kuratas'),$('#mkgndm')];
+var dialogue = { // dialogue options for characters
+  martha: [
+    "My God. I need to put an end to this senseless carnage. Moooooooo!",
+    "We have to secure a future for the calves. I won't let anyone stop me!",
+    "I can never stop until they are all destroyed. God, give me the strength to see it through.",
+    "Finally, it is over. Mooooooooooo!",
+    "A robot. I'll never let you past me, damned machine.",
+    "L-l-liam, my love. They p-p-put something in my b-brain. I'm already gone. Please end me. BZZZZZZZZZZTTTTT. KILL."
+  ],
+  liam: [
+    "I'm ready. God help us.",
+    "I need to stay strong. Time to charge ahead.",
+    "...my vision's getting darker. But I can't rest. For my family, I lay down my life.",
+    "...and so finally I can rest. Father...",
+    "My sweet Martha. They are c-c-BZZZZT-c-controlling my mind. Kill me, bef-BZZZZT-ore I h-h-h-hurt you."
+  ],
+  kuratas: [
+    "PREPARATIONS COMPLETE. ATTACK SEQUENCE INITIATED.",
+    "THREAT NEUTRALIZED. THIS KILLING IS... ...UNNECESSARY.",
+    "SYSTEM DIAGNOSTICS RETURN CRITICAL. ...IS THIS FEAR?",
+    "MISSION COMPLETE. SO WHY AM I SO SAD?",
+    "THREAT DETECTED. PLEASE, STAY BACK.",
+    "MK-GNDM, YOUR BLOODLUST IS OVERWHELMING YOU. DESIST."
+  ],
+  mkgndm: [
+    "I MUST WIN. THESE CATTLE CANNOT DEFEAT ME.",
+    "VICTORY CAN BRING JOY, BUT ONLY ANNIHILTION WILL BRING AN END.",
+    "THE LAND RUNS RED WITH BLOOD AND OIL. THE WEAK FIND SUCCOR IN PEACE, WHILE THE STRONG REVEL IN DOMINATION.",
+    "DEATH AND GLORY ARE ALL THAT HOLD MEANING. I MUST FIND MORE... MORE!",
+    "YOU CANNOT HOLD TO STAND AGAINST ME. DIE.",
+    "KURATAS. HAVE YOU BETRAYED US?"
+  ]
+}
 
-function Character(name, hp, atk, cntrAtk) { // define character constructor
+function Character(name, hp, atk, cntrAtk, dialogue) { // define character constructor
   this.name = name;
   this.hp = hp;
   this.atk = atk;
   this.cntrAtk = cntrAtk;
+  this.dialogue = dialogue;
 
   this.attack = function(enemy) { // player attack
     enemy.hp -= this.atk;
@@ -16,10 +50,10 @@ function Character(name, hp, atk, cntrAtk) { // define character constructor
   }
 }
 
-var martha = new Character("martha", 140, 5, 5); // constructing all characters
-var liam = new Character("liam", 100, 8, 8);
-var kuratas = new Character("kuratas", 200, 4, 4);
-var mkgndm = new Character("mkgndm", 120, 10, 10);
+var martha = new Character("martha", 140, 5, 5, dialogue.martha); // constructing all characters
+var liam = new Character("liam", 100, 8, 8, dialogue.liam);
+var kuratas = new Character("kuratas", 200, 4, 4, dialogue.kuratas);
+var mkgndm = new Character("mkgndm", 120, 10, 10, dialogue.mkgndm);
 
 gameStart = function(characterChoice) { // begins game process after character selection
   if (gameRunning === false) {
@@ -36,17 +70,20 @@ gameStart = function(characterChoice) { // begins game process after character s
 
       $('.title').css("display", "none");
       $('.char-prompt').css("display", "none");
-      $('body').css({"display":"flex", "align-items":"center", "justify-content":"center"});
       $('.character').addClass('no-display');
+      // move everything to enemy select screen
+      $('#player-zone').append(player);
+      $('#enemy-zone').append(...enemies);
+      $('#title-screen').addClass('no-display');
+      $('#enemy-select').removeClass('no-display');
       player.removeClass('no-display'); // display and fade in player character
       player.addClass('fadein');
-      $('.fighter-holder').append($('<div>').addClass('col-xs-4'));
-      $('.fighter-holder').append($('<div>').addClass('col-xs-5').append(enemies[0]));
       enemies.forEach((enemy) => { // display and fade in enemy characters for selection
         enemy.css({"background-color":"#252525", "color":"#bd1c00"});
         enemy.removeClass('no-display');
         enemy.addClass('fadein'); 
        });
+      $('.select-text').text(martha.dialogue[0]);
 
     }, 3000);
 
